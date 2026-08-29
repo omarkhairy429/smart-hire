255bd586f5f6879fd1934c4ac6ce6a119e3f79e5
MuhmmadMahmoud <193416769+MuhmmadMahmoud@users.noreply.github.com>
2026-08-29 19:03:42 +0300

    Add schedule interview modal and interviewer pages

---
+++ FE/src/app/core/services/interview.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { StaffResponse } from '../models/api.models';
+import {
+  DossierResponse,
+  InterviewResponse,
+  ScheduleInterviewRequest
+} from '../models/interview.models';
+
+@Injectable({ providedIn: 'root' })
+export class InterviewService {
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  /** HR: staff who can be assigned to an interview. */
+  getInterviewers(): Observable<StaffResponse[]> {
+    return this.http.get<StaffResponse[]>(`${this.apiUrl}/users/interviewers`);
+  }
+
+  scheduleInterview(
+    applicationId: string,
+    req: ScheduleInterviewRequest
+  ): Observable<InterviewResponse> {
+    return this.http.post<InterviewResponse>(
+      `${this.apiUrl}/applications/${applicationId}/interviews`,
+      req
+    );
+  }
+
+  getInterviewsByApplication(applicationId: string): Observable<InterviewResponse[]> {
+    return this.http.get<InterviewResponse[]>(
+      `${this.apiUrl}/applications/${applicationId}/interviews`
+    );
+  }
+
+  cancelInterview(interviewId: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/interviews/${interviewId}`);
+  }
+
+  getMyInterviews(): Observable<InterviewResponse[]> {
+    return this.http.get<InterviewResponse[]>(`${this.apiUrl}/interviewer/my-interviews`);
+  }
+
+  getDossier(interviewId: string): Observable<DossierResponse> {
+    return this.http.get<DossierResponse>(
+      `${this.apiUrl}/interviewer/interviews/${interviewId}/dossier`
+    );
+  }
+}
---
+++ FE/src/app/features/hr/schedule-interview/schedule-interview.component.ts
@@ -0,0 +1,85 @@
+import { Component, EventEmitter, Input, OnInit, Output, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { InterviewService } from '../../../core/services/interview.service';
+import { StaffResponse } from '../../../core/models/api.models';
+import { InterviewResponse } from '../../../core/models/interview.models';
+
+@Component({
+  selector: 'app-schedule-interview',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './schedule-interview.component.html',
+  styleUrls: ['./schedule-interview.component.css']
+})
+export class ScheduleInterviewComponent implements OnInit {
+  @Input({ required: true }) applicationId!: string;
+  @Input() candidateName = '';
+
+  @Output() closed = new EventEmitter<void>();
+  @Output() scheduled = new EventEmitter<InterviewResponse>();
+
+  interviewers: StaffResponse[] = [];
+  interviewerId = '';
+  scheduledAt = '';
+  meetingLink = '';
+
+  isLoading = true;
+  isSaving = false;
+  errorMessage = '';
+
+  constructor(
+    private interviewService: InterviewService,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit() {
+    this.interviewService.getInterviewers().subscribe({
+      next: (data) => {
+        this.interviewers = data;
+        this.isLoading = false;
+        this.cdr.markForCheck();
+      },
+      error: () => {
+        this.errorMessage = 'Could not load interviewers.';
+        this.isLoading = false;
+        this.cdr.markForCheck();
+      }
+    });
+  }
+
+  submit() {
+    if (!this.interviewerId || !this.scheduledAt || !this.meetingLink.trim()) {
+      this.errorMessage = 'Please fill in the interviewer, date and meeting link.';
+      return;
+    }
+
+    this.isSaving = true;
+    this.errorMessage = '';
+
+    this.interviewService.scheduleInterview(this.applicationId, {
+      interviewerId: this.interviewerId,
+      scheduledAt: this.scheduledAt,
+      meetingLink: this.meetingLink.trim()
+    }).subscribe({
+      next: (interview) => {
+        this.isSaving = false;
+        this.scheduled.emit(interview);
+        this.closed.emit();
+      },
+      error: (err) => {
+        this.errorMessage = err?.error?.message ?? 'Could not schedule the interview.';
+        this.isSaving = false;
+        this.cdr.markForCheck();
+      }
+    });
+  }
+
+  close() {
+    this.closed.emit();
+  }
+
+  fullName(staff: StaffResponse): string {
+    return `${staff.firstName} ${staff.lastName}`;
+  }
+}
