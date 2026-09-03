9f73d65fb4bb9cd2933baeff91d6d7191508e56e
omarkhairy429 <omar.fayed.5832@gmail.com>
2026-09-03 18:33:55 +0300

    Creating an email for the website

--- BE/src/main/java/orange/smart_hire/service/EmailService.java
+++ BE/src/main/java/orange/smart_hire/service/EmailService.java
@@ -1,6 +1,7 @@
 package orange.smart_hire.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -10,10 +11,20 @@ public class EmailService {
 
     @Autowired
     private JavaMailSender mailSender;
+    private final String fromEmail;
+
+    public EmailService(
+            JavaMailSender mailSender,
+            @Value("${spring.mail.username}") String fromEmail) {
+        this.mailSender = mailSender;
+        this.fromEmail = fromEmail;
+    }
+
+
 
     public void sendEmail(String to, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("omar.fayed.5832@gmail.com");
+        message.setFrom(fromEmail);
         message.setTo(to);
         message.setSubject(subject);
         message.setText(body);
