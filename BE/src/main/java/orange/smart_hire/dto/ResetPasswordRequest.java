31124d523f0e80dc0bcf4276eb1bcee732484b55
Youana-Joseph <162136949+Youana-Joseph@users.noreply.github.com>
2026-09-10 01:36:43 +0300

    reset password backend (#58)

---
+++ BE/src/main/java/orange/smart_hire/dto/ResetPasswordRequest.java
@@ -0,0 +1,12 @@
+package orange.smart_hire.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResetPasswordRequest {
+
+    private String token;
+    private String newPassword;
+}
